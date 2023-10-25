3ef2b5f9abccf050f7cce55ae4a7f70a0b86b66e
mgarbowski <mikolaj.garbowski@gmail.com>
2023-10-25 21:35:37 +0200

    refactor scraper

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from pprint import pprint
 
 from selenium import webdriver
+from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.select import Select
@@ -36,46 +37,46 @@ def process_row(tr_element: WebElement) -> Course:
     )
 
 
+def scrape_course_category(driver: WebDriver, select_tag_id: str, select_idx: int, btn_name: str) -> list[Course]:
+    """This is for scraping either 'thematic language courses' or 'courses in languages other than English'"""
+    courses = []
+    select_element = driver.find_elements(By.ID, select_tag_id)[select_idx]
+    select = Select(select_element)
+
+    n_languages = len(select.options)
+    for idx in range(1, n_languages):  # idx 0 is a filler
+        select_element = driver.find_elements(By.ID, select_tag_id)[select_idx]
+        select = Select(select_element)
+        select.select_by_index(idx)
+
+        button = driver.find_element(By.NAME, btn_name)
+        button.click()
+
+        table_rows = [
+            *driver.find_elements(By.CLASS_NAME, "even"),
+            *driver.find_elements(By.CLASS_NAME, "odd"),
+        ]
+        courses.extend([process_row(row) for row in table_rows])
+
+        driver.back()
+
+    return courses
+
+
+def scrape_course_list(driver: WebDriver, page_url: str) -> list[Course]:
+    driver.get(page_url)
+    select_tag_id = "Jezyki_ID_JEZYKA"
+    all_courses = [
+        *scrape_course_category(driver, select_tag_id, 0, "yt0"),
+        *scrape_course_category(driver, select_tag_id, 1, "yt1"),
+    ]
+
+    return all_courses
+
+
 def main():
-    all_courses = []
-
     driver = webdriver.Chrome()
-    driver.get(SJO_URL)
-
-    select_element = driver.find_elements(By.ID, "Jezyki_ID_JEZYKA")[0]
-    select = Select(select_element)
-
-    n_languages = len(select.options)
-    for idx in range(1, n_languages):
-        select_element = driver.find_elements(By.ID, "Jezyki_ID_JEZYKA")[0]
-        select = Select(select_element)
-        select.select_by_index(idx)
-        button = driver.find_element(By.NAME, "yt0")
-        button.click()
-
-        table_rows = driver.find_elements(By.CLASS_NAME, "even") + driver.find_elements(By.CLASS_NAME, "odd")
-        language_courses = [process_row(row) for row in table_rows]
-        all_courses.extend(language_courses)
-
-        driver.back()
-
-    ##########################
-    select_element = driver.find_elements(By.ID, "Jezyki_ID_JEZYKA")[1]
-    select = Select(select_element)
-
-    n_languages = len(select.options)
-    for idx in range(1, n_languages):
-        select_element = driver.find_elements(By.ID, "Jezyki_ID_JEZYKA")[1]
-        select = Select(select_element)
-        select.select_by_index(idx)
-        button = driver.find_element(By.NAME, "yt1")
-        button.click()
-
-        table_rows = driver.find_elements(By.CLASS_NAME, "even") + driver.find_elements(By.CLASS_NAME, "odd")
-        language_courses = [process_row(row) for row in table_rows]
-        all_courses.extend(language_courses)
-
-        driver.back()
+    all_courses = scrape_course_list(driver, SJO_URL)
 
     print(f"Collected {len(all_courses)} in total")
     pprint(all_courses)
